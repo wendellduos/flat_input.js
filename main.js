a1b7d55080769c0f27c4d8954b4c3d55a7c108d8
Wendel <wendellduos@hotmail.com>
2023-12-21 17:41:48 -0300

    added rudimentary logic

--- main.js
+++ main.js
@@ -5,53 +5,105 @@ let keybinds = {
   right: "d",
 };
 
+let framerate = 33.34;
+let playerSpeed = 10;
 let intervals = [];
+let position = {
+  x: 0,
+  y: 0,
+};
 
 let move = {
   up: function () {
     if (intervals.length === 0) {
       intervals.push(
         setInterval(() => {
-          console.log("going up");
-        }, 500)
+          position.x -= playerSpeed;
+          updatePlayerPosition();
+        }, framerate)
       );
+
+      let thisInterval = intervals.length - 1;
+
+      addEventListener("keyup", ({ key }) => {
+        const release = key.toLowerCase();
+
+        if (release === keybinds.up) {
+          clearInterval(intervals[thisInterval]);
+          intervals.splice(thisInterval, 1);
+        }
+      });
     }
-
-    let thisInterval = intervals.length - 1;
-
-    addEventListener("keyup", ({ key }) => {
-      const release = key.toLowerCase();
-
-      if (release === keybinds.up) {
-        clearInterval(intervals[thisInterval]);
-        intervals.splice(thisInterval, 1);
-
-        console.log(intervals.length);
-      }
-    });
   },
   down: function () {
     if (intervals.length === 0) {
-      interval = setInterval(() => {
-        console.log("going down");
-      }, 500);
+      intervals.push(
+        setInterval(() => {
+          position.x += playerSpeed;
+          updatePlayerPosition();
+        }, framerate)
+      );
+
+      let thisInterval = intervals.length - 1;
+
+      addEventListener("keyup", ({ key }) => {
+        const release = key.toLowerCase();
+
+        if (release === keybinds.down) {
+          clearInterval(intervals[thisInterval]);
+          intervals.splice(thisInterval, 1);
+        }
+      });
     }
   },
   left: function () {
     if (intervals.length === 0) {
-      interval = setInterval(() => {
-        console.log("going left");
-      }, 500);
+      intervals.push(
+        setInterval(() => {
+          position.y -= playerSpeed;
+          updatePlayerPosition();
+        }, framerate)
+      );
+
+      let thisInterval = intervals.length - 1;
+
+      addEventListener("keyup", ({ key }) => {
+        const release = key.toLowerCase();
+
+        if (release === keybinds.left) {
+          clearInterval(intervals[thisInterval]);
+          intervals.splice(thisInterval, 1);
+        }
+      });
     }
   },
   right: function () {
     if (intervals.length === 0) {
-      interval = setInterval(() => {
-        console.log("going right");
-      }, 500);
+      intervals.push(
+        setInterval(() => {
+          position.y += playerSpeed;
+          updatePlayerPosition();
+        }, framerate)
+      );
+
+      let thisInterval = intervals.length - 1;
+
+      addEventListener("keyup", ({ key }) => {
+        const release = key.toLowerCase();
+
+        if (release === keybinds.right) {
+          clearInterval(intervals[thisInterval]);
+          intervals.splice(thisInterval, 1);
+        }
+      });
     }
   },
 };
+
+function updatePlayerPosition() {
+  player.style.top = `${position.x}px`;
+  player.style.left = `${position.y}px`;
+}
 
 addEventListener("keydown", ({ key }) => {
   const input = key.toLowerCase();
